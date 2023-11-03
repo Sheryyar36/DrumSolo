d21625be7a04eddac9dfaf0c8b8028bc4a7ecaae
Sheheryar Saeed <sheryyar36@gmail.com>
2023-11-03 09:25:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+var buttonSelector = document.querySelectorAll(".drum");
+
+for(var i = 0; i < buttonSelector.length; i++){
+
+    console.log(i);
+
+    buttonSelector[i].addEventListener("click", function() {
+        
+        var key = this.innerHTML;
+
+        makeSound(key);
+
+    });
+
+};
+
+document.addEventListener("keydown", function(event){
+
+    makeSound(event.key);
+
+});
+
+function makeSound(key){
+
+    switch(key){
+
+        case "s": 
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "d": 
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "f": 
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j": 
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "k": 
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "l": 
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "spacebar": 
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        
+        case " ": 
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;    
+
+        default: 
+            console.log(key + " button was pressed");
+
+    };
+
+};
